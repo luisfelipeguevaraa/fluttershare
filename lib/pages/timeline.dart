8751c1cea963d5966d3fb00c0d1bc4b7d7c6013c
Luis Guevara <luisfelipe.guevaraa@gmail.com>
2019-10-27 11:02:44 -0500

    firebase get document and by ID

--- lib/pages/timeline.dart
+++ lib/pages/timeline.dart
@@ -1,7 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import '../widgets/header.dart';
 import '../widgets/progress.dart';
+
+final usersRef = Firestore.instance.collection('users');
 
 class Timeline extends StatefulWidget {
   @override
@@ -9,6 +12,34 @@ class Timeline extends StatefulWidget {
 }
 
 class _TimelineState extends State<Timeline> {
+
+  @override
+  void initState() {
+    //getUsers();
+    getUserById();
+    super.initState();
+  }
+
+  getUsers(){
+    usersRef.getDocuments().then((QuerySnapshot snapshot){
+      snapshot.documents.forEach((DocumentSnapshot doc){
+        print(doc.data);
+        print(doc.documentID);
+        print(doc.exists);
+      });
+    });
+  }
+
+  getUserById() async {
+    final String id = "vFdeUyG1bVxInjSEgRqn";
+    final DocumentSnapshot doc = await usersRef.document(id).get();
+    
+      print(doc.data);
+      print(doc.documentID);
+      print(doc.exists);
+    
+  }
+
   @override
   Widget build(context) {
     return Scaffold(
