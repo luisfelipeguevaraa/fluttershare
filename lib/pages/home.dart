19c2d57741d53467496fb18c3bcc5efecedb51b6
Luis Guevara <luisfelipe.guevaraa@gmail.com>
2019-11-03 12:56:33 -0500

    pantalla upload design

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -126,7 +126,7 @@ class _HomeState extends State<Home> {
             onPressed: logout,
           ),
           ActivityFeed(),
-          Upload(),
+          Upload(currentUser: currentUser),
           Search(),
           Profile(),
         ],
