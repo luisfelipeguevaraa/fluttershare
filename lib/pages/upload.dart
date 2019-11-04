e2102919d878a6b847bfbf6fa1856a9d1a933341
Luis Guevara <luisfelipe.guevaraa@gmail.com>
2019-11-03 22:10:57 -0500

    31 geolocalizacion

--- lib/pages/upload.dart
+++ lib/pages/upload.dart
@@ -7,6 +7,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:fluttershare/models/user.dart';
 import 'package:fluttershare/pages/home.dart';
 import 'package:fluttershare/widgets/progress.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:image/image.dart' as Im;
@@ -251,7 +252,7 @@ class _UploadState extends State<Upload> {
                   borderRadius: BorderRadius.circular(30.0), 
                   ),
                   color: Colors.blue,
-                  onPressed: () => print('get user location'),
+                  onPressed: getUserLocation,
                   icon: Icon(
                     Icons.my_location,
                     color: Colors.white,
@@ -261,6 +262,18 @@ class _UploadState extends State<Upload> {
         ],
       ),
     );
+  }
+
+  getUserLocation() async {
+    Position position = await Geolocator().getCurrentPosition
+    (desiredAccuracy: LocationAccuracy.high);
+    List<Placemark> placemarks = await Geolocator()
+    .placemarkFromCoordinates(position.latitude, position.longitude);
+    Placemark placemark = placemarks[0];
+     String completeAddress = '${placemark.subThoroughfare} ${placemark.thoroughfare}, ${placemark.subLocality} ${placemark.locality}, ${placemark.subAdministrativeArea}, ${placemark.administrativeArea} ${placemark.postalCode}, ${placemark.country}';
+     print(completeAddress);
+     String formattedAddress = "${placemark.locality}, ${placemark.country}";
+     locationController.text = formattedAddress;
   }
 
   @override
