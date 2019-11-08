a4b80f4fe99cdb183a8559734bb20f079c8e293d
Luis Guevara <luisfelipe.guevaraa@gmail.com>
2019-11-08 00:01:26 -0500

    44 funcionalidad de comentarios

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import 'pages/home.dart';
 
 void main() {
+  Firestore.instance.settings(timestampsInSnapshotsEnabled: true).then((_){
+    print("Timestamps enable in snapshots\n");
+  }, onError: (_){
+    print("Error enabling timestamps in snapshots\n");
+  });
   runApp(MyApp());
 }
 
--- lib/pages/comments.dart
+++ lib/pages/comments.dart
@@ -1,5 +1,10 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttershare/pages/home.dart';
 import 'package:fluttershare/widgets/header.dart';
+import 'package:fluttershare/widgets/progress.dart';
+import 'package:timeago/timeago.dart' as timeago;
 
 class Comments extends StatefulWidget {
 
@@ -35,7 +40,34 @@ class CommentsState extends State<Comments> {
   });
 
   buildComments(){
-    return Text("Comment");
+    return StreamBuilder(
+      stream: commentsRef.document(postId).collection('comments')
+      .orderBy("timestamp", descending: false).snapshots(),
+      builder: (context, snapshot){
+        if(!snapshot.hasData){
+          return circularProgress();
+        } 
+        List<Comment> comments = [];
+        snapshot.data.documents.forEach((doc){
+          comments.add(Comment.fromDocument(doc));
+        });
+        return ListView(children: comments,);
+      }
+    );
+  }
+
+  addComment(){
+    commentsRef
+    .document(postId)
+    .collection("comments")
+    .add({
+      "username" : currentUser.username,
+      "comment": commentController.text,
+      "timestamp": timestamp,
+      "avatarUrl": currentUser.photoUrl,
+      "userId": currentUser.id,
+    });
+    commentController.clear();
   }
 
   @override
@@ -52,7 +84,7 @@ class CommentsState extends State<Comments> {
               decoration: InputDecoration(labelText: "Writea comment..."),              
             ),
             trailing: OutlineButton(
-              onPressed: () => print('add comment'),
+              onPressed: addComment,
               borderSide: BorderSide.none,
               child: Text("Post"),
 
@@ -65,9 +97,46 @@ class CommentsState extends State<Comments> {
 }
 
 class Comment extends StatelessWidget {
+  final String username;
+  final String userId;
+  final String avatarUrl;
+  final String comment;
+  final Timestamp timestamp;
+
+  Comment({
+    this.username,
+  this.userId,
+  this.avatarUrl,
+  this.comment,
+  this.timestamp,
+  });
+
+  factory Comment.fromDocument(DocumentSnapshot doc){
+    return Comment(
+      username: doc['username'],
+      userId: doc['userId'],
+      avatarUrl: doc['avatarUrl'],
+      comment: doc['comment'],
+      timestamp: doc['timestamp'],
+    );
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return Text('Comment');
+    return Column(
+      children: <Widget>[
+        ListTile(
+          title: Text(comment),
+          leading: CircleAvatar(
+            backgroundImage: CachedNetworkImageProvider(avatarUrl),
+          ),
+          subtitle: Text(timeago.format(timestamp.toDate())),
+        ),
+        Divider(),
+
+      ],
+    );
   }
 }
 
